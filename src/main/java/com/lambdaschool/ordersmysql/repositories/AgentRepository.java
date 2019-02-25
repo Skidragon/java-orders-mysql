930ed9e6153f6b50e0d2e1225cd15a6bd913aa47
Simon Davis <simon_freelance@hotmail.com>
2019-02-25 16:22:36 -0500

    Starting repositories for orders, customers, and agents created.

---
+++ src/main/java/com/lambdaschool/ordersmysql/repositories/AgentRepository.java
@@ -0,0 +1,6 @@
+package com.lambdaschool.ordersmysql.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentRepository extends JpaRepository<AgentRepository, Long> {
+}
