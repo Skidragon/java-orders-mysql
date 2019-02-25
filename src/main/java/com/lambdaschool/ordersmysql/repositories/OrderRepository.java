4c8cf8ff711016889ce053e472d3e940be9fecf5
Simon Davis <simon_freelance@hotmail.com>
2019-02-25 17:38:42 -0500

    Order routes created without docs

--- src/main/java/com/lambdaschool/ordersmysql/repositories/OrderRepository.java
+++ src/main/java/com/lambdaschool/ordersmysql/repositories/OrderRepository.java
@@ -2,6 +2,11 @@ package com.lambdaschool.ordersmysql.repositories;
 
 import com.lambdaschool.ordersmysql.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query(value="SELECT * FROM orders o WHERE o.ordernum = :ordernum", nativeQuery = true)
+    Order findOrderByOrderNumber(@Param("ordernum") long ordernum);
 }
