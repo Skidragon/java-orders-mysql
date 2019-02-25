5e7af6c3dfc3df5c9fd78c3e46848bda3b9d51ff
Simon Davis <simon_freelance@hotmail.com>
2019-02-25 16:40:05 -0500

    Fixed generic for AgentRepository

--- src/main/java/com/lambdaschool/ordersmysql/repositories/AgentRepository.java
+++ src/main/java/com/lambdaschool/ordersmysql/repositories/AgentRepository.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.ordersmysql.repositories;
 
+import com.lambdaschool.ordersmysql.models.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AgentRepository extends JpaRepository<AgentRepository, Long> {
+public interface AgentRepository extends JpaRepository<Agent, Long> {
 }
