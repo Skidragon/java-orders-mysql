18ec900c04fe2a0b187fac6bee1385edd0c0e40b
Simon Davis <simon_freelance@hotmail.com>
2019-02-25 17:38:58 -0500

    Customer routes created without docs

---
+++ src/main/java/com/lambdaschool/ordersmysql/controllers/CustomerController.java
@@ -0,0 +1,68 @@
+package com.lambdaschool.ordersmysql.controllers;
+
+import com.lambdaschool.ordersmysql.models.Customer;
+import com.lambdaschool.ordersmysql.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path={"/customers"}, produces= MediaType.APPLICATION_JSON_VALUE)
+public class CustomerController {
+    @Autowired
+    CustomerRepository custRepo;
+// Apply Swagger documentation later
+//    @ApiOperation(value = "List All Customers", response = List.class)
+//    @ApiResponses(value ={
+//            @ApiResponse(code = 200, message = "Successfully recetrieve list"),
+//            @ApiResponse(code = 401, message = "You are not authorized to the view the resource"),
+//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+//    })
+    @GetMapping("")
+    public List<Customer> allCustomers() {
+        return custRepo.findAll();
+    }
+
+    @GetMapping("custcode/{customercode}")
+    public Customer findCustomerByCode(@PathVariable long customercode) {
+        return custRepo.findCustomerByCode(customercode);
+    }
+
+    @GetMapping("order")
+    public List<Object[]> allCustomersWithOrders() {
+        return custRepo.findAllCustomersWithOrders();
+    }
+    @GetMapping("name/{name}")
+    public List<Object[]> getCustomerByName(@PathVariable String name) {
+        return custRepo.findCustomerByNameWithOrders(name);
+    }
+    @GetMapping("order/{name}")
+    public List<Object[]> getCustomerByNameWithAllOrders(@PathVariable String name) {
+        return custRepo.findCustomerByNameWithOrders(name);
+    }
+    @PutMapping("custcode/{customercode}")
+    public Customer updateCustomer(@RequestBody Customer updatedCustomer, @PathVariable long customercode) throws URISyntaxException {
+        Optional<Customer> foundCustomer = custRepo.findById(customercode);
+        if(foundCustomer.isPresent()) {
+            updatedCustomer.setCustomercode(customercode);
+            custRepo.save(updatedCustomer);
+            return updatedCustomer;
+        }
+        return null;
+    }
+
+    @DeleteMapping("custcode/{customercode}")
+    public Customer deleteCustomer(@PathVariable long customercode) {
+        Customer foundCustomer = custRepo.findCustomerByCode(customercode);
+        if (foundCustomer != null) {
+            custRepo.deleteById(customercode);
+            return foundCustomer;
+        }
+        return null;
+    }
+}
--- src/main/java/com/lambdaschool/ordersmysql/repositories/CustomerRepository.java
+++ src/main/java/com/lambdaschool/ordersmysql/repositories/CustomerRepository.java
@@ -2,6 +2,20 @@ package com.lambdaschool.ordersmysql.repositories;
 
 import com.lambdaschool.ordersmysql.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query(value ="SELECT * FROM customers c WHERE c.customercode = :customercode", nativeQuery = true)
+    Customer findCustomerByCode(@Param("customercode") long customercode);
+
+    @Query(value="SELECT c.name, o.ordernum FROM customers AS c INNER JOIN orders AS o ON c.customercode = o.customercode ORDER BY c.customercode ASC", nativeQuery = true)
+    List<Object[]> findAllCustomersWithOrders();
+
+    @Query(value = "SELECT c.name, o.ordernum FROM customers AS c INNER JOIN orders AS o WHERE c.name Like :name", nativeQuery = true)
+    List<Object[]> findCustomerByNameWithOrders(@Param("name") String name);
+
+    List<Customer> findCustomerByName(String name);
 }
