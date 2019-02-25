da4e098f84eef89b0390459aef8def94ad80768a
Simon Davis <simon_freelance@hotmail.com>
2019-02-25 16:10:34 -0500

    Initial setup

---
+++ src/main/java/com/lambdaschool/ordersmysql/OrdersMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.ordersmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersMysqlApplication.class, args);
+    }
+
+}
